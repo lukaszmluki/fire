5ad8e1bc94b96b333a22b411b3abb87b86afe769
Lukasz Marek <lukasz.m.luki2@gmail.com>
2013-12-31 17:04:14 +0100

    update.sh exit on configure error

--- contrib/update.sh
+++ contrib/update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+die() {
+  echo -e "\nContrib contiguration failed";
+  exit 1;
+}
+
 if [ ! -d "ffmpeg" ]; then
   git clone https://github.com/lukaszmluki/ffmpeg.git ffmpeg
   pushd ffmpeg
@@ -10,7 +15,7 @@ pushd ffmpeg
 git fetch origin
 git checkout fire
 git reset --hard origin/fire
-./configure  --prefix=../../external --disable-programs --disable-doc --enable-libpulse --enable-libssh
+./configure  --prefix=../../external --disable-programs --disable-doc --enable-libpulse --enable-libssh || die
 make -j16
 make install
 popd
