ba54468e5e80817354aefadfa6fe94ead00bab79
Lukasz Marek <lukasz.m.luki2@gmail.com>
2014-07-21 01:26:42 +0200

    playlist view: dummy context menu

--- src/playlist/playlist_view.cpp
+++ src/playlist/playlist_view.cpp
@@ -6,7 +6,12 @@
  */
 
 #include "playlist_view.h"
+#include <QMenu>
+#include <QDebug>
+#include <QPoint>
+#include <QModelIndex>
 #include "playlist_data_model.h"
+#include "playlist_item.h"
 
 PlaylistView::PlaylistView(QWidget *parent) :
     QTreeView(parent)
@@ -16,4 +21,39 @@ PlaylistView::PlaylistView(QWidget *parent) :
     setAlternatingRowColors(true);
     setModel(new PlaylistDataModel(this));
     expandToDepth(0);
+
+    setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(this, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(customContextMenuRequest(const QPoint &)));
+}
+
+void PlaylistView::showDirectoryContextMenu(const QModelIndex &index, const QPoint &point) const
+{
+    PlaylistItem *item = static_cast<PlaylistItem *>(index.internalPointer());
+    QMenu *contextMenu = new QMenu();
+    contextMenu->addAction("Add as shortcut");
+    contextMenu->addAction("Add as shortcut \"" + item->name() + "\"");
+    contextMenu->exec(mapToGlobal(point) + QPoint(3, 3));
+    delete contextMenu;
+}
+
+void PlaylistView::showFileContextMenu(const QModelIndex &index, const QPoint &point) const
+{
+    PlaylistItem *item = static_cast<PlaylistItem *>(index.internalPointer());
+    Q_UNUSED(item)
+    QMenu *contextMenu = new QMenu();
+    contextMenu->addAction("Play");
+    contextMenu->exec(mapToGlobal(point) + QPoint(3, 3));
+    delete contextMenu;
+}
+
+void PlaylistView::customContextMenuRequest(const QPoint &point) const
+{
+    QModelIndex index = indexAt(point);
+    if (index.isValid()) {
+        PlaylistItem *item = static_cast<PlaylistItem *>(index.internalPointer());
+        if (item->itemType() == PlaylistItem::PLAYLIST_ITEM_DIRECTORY)
+            showDirectoryContextMenu(index, point);
+        else if (item->itemType() == PlaylistItem::PLAYLIST_ITEM_FILE)
+            showFileContextMenu(index, point);
+    }
 }
--- src/playlist/playlist_view.h
+++ src/playlist/playlist_view.h
@@ -10,16 +10,24 @@
 
 #include <QTreeView>
 
+class QModelIndex;
+
 class PlaylistView : public QTreeView
 {
     Q_OBJECT
 public:
     explicit PlaylistView(QWidget *parent = 0);
 
+private:
+    void showDirectoryContextMenu(const QModelIndex &index, const QPoint &point) const;
+    void showFileContextMenu(const QModelIndex &index, const QPoint &point) const;
+
 signals:
 
 public slots:
 
+private slots:
+    void customContextMenuRequest(const QPoint &point) const;
 };
 
 #endif /* SRC_PLAYLIST_H */
